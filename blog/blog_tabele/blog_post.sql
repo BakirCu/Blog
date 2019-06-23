3330369d13933f8a5e08d732c5ebf443caf08429
Bakir <bakir.feniks@gmail.com>
2019-06-23 17:16:46 +0200

    poslao novu bazu

--- blog/blog_tabele/blog_post.sql
+++ blog/blog_tabele/blog_post.sql
@@ -25,12 +25,12 @@ DROP TABLE IF EXISTS `posts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `posts` (
-  `post` mediumtext NOT NULL,
   `title` varchar(45) NOT NULL,
   `time_post` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `post_id` int(11) NOT NULL AUTO_INCREMENT,
+  `post` varchar(13000) NOT NULL,
   PRIMARY KEY (`post_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +39,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES ('sadsadasdas','bakirsdasd','2019-06-21 09:28:03',1),('sdasdasd','bakir','2019-06-21 09:28:11',2),('sadawdqwdqwdwq','bakir','2019-06-21 09:28:17',3),('werqwrqwer','nemanja','2019-06-21 09:31:08',4),('k;lk;ll;kl','nemanja','2019-06-21 09:31:54',5),('k;lk;ll;kl','nemanja','2019-06-21 09:32:48',6),('jlkjkljkljkl','timur','2019-06-21 09:33:55',7),('jlkjkljkljkl','timur','2019-06-21 09:33:55',8),('oipoipoipo','bakirsdasd','2019-06-21 09:35:32',9),('jjjj','bakir','2019-06-21 09:44:27',10),('mmmmm','bakir','2019-06-21 09:46:04',11);
+INSERT INTO `posts` VALUES ('bakir','2019-06-23 14:44:56',16,'jasklasjaklas,,sklka'),('bakir','2019-06-23 14:57:01',17,'sdfsfdfsdf'),('nemanja','2019-06-23 14:57:24',18,',as.d,as.d,.asd,a,sds');
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-06-21 11:51:12
+-- Dump completed on 2019-06-23 17:15:58
