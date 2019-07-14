bb990f732a1592d899bd97cbcc780e7eead4b683
Bakir <bakir.feniks@gmail.com>
2019-07-14 18:24:56 +0200

    poceo serdjivanje home funkcije

--- blog/server.py
+++ blog/server.py
@@ -11,71 +11,20 @@ class MojSajt(resource.Resource):
 
     @staticmethod
     def home(request):
-        request.setHeader("Content-Type", "text/html")
-        with open('templates/home.html', 'r') as file:
-            template_smal = file.read()
-            all_posts = Storage.select_posts(0, 7)
-            all_posts_list = []
-
-            for post in all_posts:
-
-                post_dict = {'title': post[0],
-                             'post': post[3],
-                             'date': str(post[1]),
-                             'id': str(post[2])
-                             }
-                all_posts_list.append(post_dict)
-            data_smal = {'posts': all_posts_list}
-            post_len = Storage.post_len()
-
-            if post_len > 7:
-                data_smal['next'] = '<a href="/get_posts?page=2&page_size=7"> Next </a>'
-                data_smal['previous'] = ''
-
-            else:
-                data_smal['next'] = ''
-                data_smal['previous'] = ''
-            template_content = render(template_smal, data_smal)
-
-            return Post.read_base_template(template_content)
+        return MojSajt.get_posts_from_to(request, 1, 7)
 
     @staticmethod
     def get_posts(request):
         page = request.args[b"page"][0].decode('UTF-8')
         page_size = request.args[b"page_size"][0].decode('UTF-8')
-        if page == '1' and page_size == '7':
-            request.path = b"/"
-            # ovde sa bukvalno kopirao sve sa putanje '/', a to znam da nije dobro,
-            #  ali ne znam kao da kad mi je page =1 ponovo procitam sve sa request.path == b"/":
-            request.setHeader("Content-Type", "text/html")
-            with open('templates/home.html', 'r') as file:
-                template_smal = file.read()
-                all_posts = Storage.select_posts(0, 7)
-                all_posts_list = []
 
-                for post in all_posts:
+        return MojSajt.get_posts_from_to(request, page, page_size)
 
-                    post_dict = {'title': post[0],
-                                 'post': post[3],
-                                 'date': str(post[1]),
-                                 'id': str(post[2])
-                                 }
-                    all_posts_list.append(post_dict)
-                data_smal = {'posts': all_posts_list}
-                post_len = Storage.post_len()
-
-                if post_len > 7:
-                    data_smal['next'] = '<a href="/get_posts?page=2&page_size=7"> Next </a>'
-                    data_smal['previous'] = ''
-
-                else:
-                    data_smal['next'] = ''
-                template_content = render(template_smal, data_smal)
-
-                return Post.read_base_template(template_content)
-
+    @staticmethod
+    def get_posts_from_to(request, page, page_size):
         page_num = int(page)
         page_size_num = int(page_size)
+
         if page_num < 1:
             template, data = InputError.raise_error(
                 'Page must be pozitive nuber')
@@ -92,9 +41,10 @@ class MojSajt(resource.Resource):
             page_previous_str, str(page_size_num))
         with open('templates/home.html', 'r') as file:
             template_smal = file.read()
-            all_posts = Storage.select_posts((page_num-1)*page_size_num, 7)
+            all_posts = Storage.select_posts(
+                (page_num-1)*page_size_num, page_size_num)
 
-            if len(all_posts) < 7:
+            if len(all_posts) < page_previous_num:
                 link_next_page = ''
             if not all_posts:
                 template, data = InputError.raise_error(
@@ -164,16 +114,20 @@ class MojSajt(resource.Resource):
                 return Post.read_base_template(template_content)
 
     def render_GET(self, request):
+        dinamic_rouds_dict = {b"/": MojSajt.home,
+                              b"/get_posts": MojSajt.get_posts,
+                              b"/post_added": MojSajt.post_added,
+                              b"/view_post": MojSajt.view_post}
 
-        route_choice_dict = {
+        static_rouds_dict = {
             b"/new_post": ('templates/new_post.html', "text/html"),
         }
 
-        if request.path in route_choice_dict:
+        if request.path in static_rouds_dict:
             try:
                 request.setHeader(
-                    "Content-Type", route_choice_dict[request.path][1])
-                with open(route_choice_dict[request.path][0], 'r') as file:
+                    "Content-Type", static_rouds_dict[request.path][1])
+                with open(static_rouds_dict[request.path][0], 'r') as file:
                     template_content = file.read()
                 return Post.read_base_template(template_content)
             except MySQLError as err:
@@ -181,17 +135,8 @@ class MojSajt(resource.Resource):
                 template_content = render(template, data)
                 return Post.read_base_template(template_content)
 
-        elif request.path == b"/":
-            return MojSajt.home(request)
-
-        elif request.path == b"/get_posts":
-            return MojSajt.get_posts(request)
-
-        elif request.path == b"/post_added":
-            return MojSajt.post_added(request)
-
-        elif request.path == b"/view_post":
-            return MojSajt.view_post(request)
+        elif request.path in dinamic_rouds_dict:
+            return dinamic_rouds_dict[request.path](request)
 
         template, data = InputError.raise_error('Unknown rout')
         template_content = render(template, data)
